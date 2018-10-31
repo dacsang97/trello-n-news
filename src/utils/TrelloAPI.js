09c470878b52caabf62c4803344f9a0338d647d7
Evan Nguyen <dacsang97@gmail.com>
2018-10-31 16:19:25 +0700

    chore - change title

--- src/utils/TrelloAPI.js
+++ src/utils/TrelloAPI.js
@@ -5,7 +5,6 @@ let TOKEN = null
 export const login = (callback, err) => {
   if (window && window.Trello) {
     window.Trello.authorize({
-      type: 'popup',
       name: 'Trello & News',
       scope: {
         read: 'true',
